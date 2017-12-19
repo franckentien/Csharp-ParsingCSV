af8517e2c1509e1cb2d3d79e79d6bbf28378979a
franckentien <franckentien@hotmail.fr>
2017-12-19 12:32:04 +0100

    Create structure for read file
    Read file and check file fonction
    Need to finish check file function

--- ParsingCSV/ParsingCsv.cs
+++ ParsingCSV/ParsingCsv.cs
@@ -1,31 +1,100 @@
-﻿namespace ParsingCSV
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ParsingCsv.cs" company="Franck CAYZAC">
+//   MIT
+// </copyright>
+// <summary>
+//   The parsing csv.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ParsingCSV
 {
     using System;
     using System.IO;
-
+    
     /// <summary>
     /// The parsing csv.
     /// </summary>
     public class ParsingCsv
     {
+        /// <summary>
+        /// The path.
+        /// </summary>
+        private static readonly string Path = Directory.GetCurrentDirectory();
+
+        private static int nbValueLine;
+
+
+        /// <summary>
+        /// The main.
+        /// </summary>
+        /// <param name="args">
+        /// The args.
+        /// </param>
         public static void Main(string[] args)
         {
-            ReadCsv();
+            ReadCsv("sample.csv");
+            Console.ReadLine();
         }
 
-        private static void ReadCsv()
+        /// <summary>
+        /// The read csv.
+        /// </summary>
+        /// <param name="fileName">
+        /// The file name.
+        /// </param>
+        private static void ReadCsv(string fileName)
         {
-            string path = Directory.GetCurrentDirectory();
+  
+            // get file content
+            string[] lines = System.IO.File.ReadAllLines(Path + "/" + fileName, System.Text.Encoding.GetEncoding("iso-8859-1"));
 
-            Console.WriteLine(path);
-            Console.ReadLine();
-            /*string[] lines = System.IO.File.ReadAllLines();
+            Checkfile(lines);
+
+
+            // display file content in console for test.
             foreach (var line in lines)
             {
-                Console.WriteLine(line);
-            }*/
+                var values = line.Split(';');
+
+                //Console.WriteLine("New line");
+
+                foreach (var value in values)
+                {
+                    //Console.WriteLine("Value: ");
+                    //Console.WriteLine(value);
+                }
+            }
         }
 
+        /// <summary>
+        /// check if all line have the same number of value
+        /// </summary>
+        /// <returns>
+        /// The <see cref="bool"/>.
+        /// </returns>
+        private static bool Checkfile(string [] lines)
+        {
+
+            var splitedLine = lines[0].Split(';');
+            nbValueLine = splitedLine.Length;
+
+            foreach (var line in lines)
+            {
+                var values = line.Split(';');
+
+                foreach (var value in values)
+                {
+                    //TODO Check the number of value and return true or false 
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// The write csv.
+        /// </summary>
         private static void WriteCsv()
         {
 
