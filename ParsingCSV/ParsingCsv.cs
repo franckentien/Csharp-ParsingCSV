6b66f01b039a6a36a6170ef465973b6778613b86
franckentien <franckentien@hotmail.fr>
2017-12-19 11:38:20 +0100

    Create project with local exe

---
+++ ParsingCSV/ParsingCsv.cs
@@ -0,0 +1,34 @@
+﻿namespace ParsingCSV
+{
+    using System;
+    using System.IO;
+
+    /// <summary>
+    /// The parsing csv.
+    /// </summary>
+    public class ParsingCsv
+    {
+        public static void Main(string[] args)
+        {
+            ReadCsv();
+        }
+
+        private static void ReadCsv()
+        {
+            string path = Directory.GetCurrentDirectory();
+
+            Console.WriteLine(path);
+            Console.ReadLine();
+            /*string[] lines = System.IO.File.ReadAllLines();
+            foreach (var line in lines)
+            {
+                Console.WriteLine(line);
+            }*/
+        }
+
+        private static void WriteCsv()
+        {
+
+        }
+    }
+}
